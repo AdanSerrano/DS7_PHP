a8b3014eec8304857df47a398dc27a47576729cd
AdanSerrano <adan.serrano@utp.ac.pa>
2023-09-13 21:38:27 -0500

    modify codig

--- lab5/lab52.php
+++ lab5/lab52.php
@@ -1,14 +1,25 @@
 <?php
-if (is_uploaded_file($_FILES['MAX_FILE_SIZE']['tmp_name'])) {
+if (is_uploaded_file($_FILES['nombre_archivo_cliente']['tmp_name'])) {
     $nombreDirectorio = "archivos/";
-    $nombrearchivo = $_FILES['MAX_FILE_SIZE']['name'];
+    $nombrearchivo = $_FILES['nombre_archivo_cliente']['name'];
     $nombreCompleto = $nombreDirectorio . $nombrearchivo;
-    if (is_file($nombreCompleto <= 1048576)) {
+    $extensionFile = array("jpg", "gif", "png", "doc", "pdf", "zip", "rar", "txt");
+    $tamaño = 1048576;
+
+    if (is_file($nombreCompleto <= $tamaño)) {
         $idUnico = time();
         $nombrearchivo = $idUnico . "-" . $nombrearchivo;
         echo "Archivo repetido, se cambiara el nombre a $nombrearchivo <br><br>";
+
+        $extension = strtolower(pathinfo($nombrearchivo, PATHINFO_EXTENSION));
+
+        if (in_array($extension, $extensionFile)) {
+            echo "La extensión del archivo no es válida o no se ha subido ningún archivo <br />";
+        } else {
+            echo "El archivo ha sido cargado correctamente <br />";
+        }
     }
-    move_uploaded_file($_FILES['MAX_FILE_SIZE']['tmp_name'], $nombreDirectorio . $nombrearchivo);
+    move_uploaded_file($_FILES['nombre_archivo_cliente']['tmp_name'], $nombreDirectorio . $nombrearchivo);
     echo "El archivo se ha subido satisfactoriamente al directorio $nombreDirectorio <br>";
 } else
     echo "No se ha podido subir el archivo <br>";
