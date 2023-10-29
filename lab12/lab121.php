3af8df0367c6e50c55c4236feeb427e76b4a9561
AdanSerrano <adan.serrano@utp.ac.pa>
2023-10-29 11:59:35 -0500

    first commit lab12

---
+++ lab12/lab121.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio 121</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+
+<body>
+    <h1>Manejo de sesiones</h1>
+    <h2>Paso 1: se crea la variable de sesión y se almacena</h2>
+
+    <?php
+    $var = "ejemplo sesiones";
+    $_SESSION['var'] = $var;
+    echo "<p>Valor de la variable de sesión: " . $var . "</p>";
+
+    ?>
+    <a href="lab122.php">Paso 2</a>
+
+
+</body>
+
+</html>
